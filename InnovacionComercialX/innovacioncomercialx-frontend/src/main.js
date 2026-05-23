66f8584cd2602aca48234d00a465e53686017622
ACSERNE <valdezwyclef@gmail.com>
2026-05-23 21:47:24 +0000

    Reestructura final: backend y frontend organizados en carpeta raíz

--- InnovacionComercialX/innovacioncomercialx-frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "./components/Navbar.js";
-import HomePage from "./pages/HomePage.js";
-
-document.getElementById("app").innerHTML = `
-  ${Navbar()}
-  <main class="container">
-    ${HomePage()}
-  </main>
-`;
--- InnovacionComercialX/innovacioncomercialx-frontend/src/pages/HomePage.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return `
-    <section>
-      <h2>Bienvenido a la Tienda Online</h2>
-      <p>Explora nuestros productos y realiza tus compras fácilmente.</p>
-    </section>
-  `;
-}
